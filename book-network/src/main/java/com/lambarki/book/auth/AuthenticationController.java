651499b9d65d38e39358a54d458157e1792c94fa
Yassine Lambarki <y.lambarki@etu.enset-media.ac.ma>
2024-04-14 02:09:57 +0100

    first commit

---
+++ book-network/src/main/java/com/lambarki/book/auth/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.lambarki.book.auth;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.ACCEPTED;
+
+@RestController
+@RequestMapping("auth")
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    @ResponseStatus(ACCEPTED)
+    public ResponseEntity<?> register(
+            @RequestBody @Valid registrationRequest request
+    ) {
+        service.register(request);
+        return ResponseEntity.accepted().build();
+
+    }
+}
---
+++ book-network/src/main/java/com/lambarki/book/auth/AuthenticationService.java
@@ -0,0 +1,9 @@
+package com.lambarki.book.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public void register(registrationRequest request) {
+    }
+}
